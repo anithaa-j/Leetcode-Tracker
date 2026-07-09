1cf7eb8d357b6189cac7b89a33f8a25e43898df1
anithaa-j <anitha29j@gmail.com>
2026-07-09 15:11:26 +0530

    Create 404-SumOfLeftLeaves

---
+++ 404-SumOfLeftLeaves/404-SumOfLeftLeaves.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:11:23 PM
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.left != null &&
+            root.left.left == null &&
+            root.left.right == null) {
+            sum += root.left.val;
+        } else {
+            sum += sumOfLeftLeaves(root.left);
+        }
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
